c9ebacf02c3272c9fb1db7ff4cfcbdb5c17fb33a
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-03-02 20:05:35 -0600

    update pal aux

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require("path");
+var HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contentHash].js",
         path: path.resolve(__dirname,"dist")
     },
+    plugins:[
+        new HtmlWebPackPlugin({
+        template: "index.html",
+    })],
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: ["style-loader","css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            }
+        ]
+    }
 
 };
