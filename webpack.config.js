da72cf1f3d76dba1c227cd36b4ee5c7f1572c087
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-03-03 19:07:55 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.[contentHash].js",
+        filename: "main.[hash].js",
         path: path.resolve(__dirname,"dist")
     },
     plugins:[
