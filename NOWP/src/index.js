6d27e08184d84ab1ad994f1ffa61be30121baead
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-03-08 12:43:49 -0600

    n

---
+++ NOWP/src/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+import "./alert.js";
+import "./1B.html"
---
+++ NOWP/src/whatJusthappened/script.js
@@ -0,0 +1,25 @@
+//Inicio
+window.onload = function(){
+    //Variables para funcionamiento del chat
+    var contenedor = document.getElementById('contenedor');
+    //---------------------------------Atributos del chat---------------------------------
+    //Fondo
+    background.style.backgroundColor = '#84cdee';
+    //Contenedor
+    contenedor.style.borderRadius       = '20px';
+    contenedor.style.border             = "3px solid black";
+    contenedor.style.backgroundColor    = '#dcf0fa';
+    contenedor.style.height             = '200px';
+    contenedor.style.width              = '400px';
+    //-posision
+    contenedor.style.position           = 'relative';
+    contenedor.style.left               = '45%';
+    contenedor.style.bottom             = '50%';
+    
+
+    
+
+
+    
+
+};
