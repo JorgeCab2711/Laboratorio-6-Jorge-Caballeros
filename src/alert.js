5737a705d7c29e097046ff5168ee8c6a14246098
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-03-15 13:15:18 -0600

    ending almost babel

--- src/alert.js
+++ src/alert.js
@@ -1,1 +1,20 @@
-alert('helloooooooo');
+window.onload = function() {
+    var footer = document.createElement('footer');
+    document.body.appendChild(footer);
+
+
+    var element = document.createElement('a');
+    element.href = '../../index.html';
+    element.style.position = 'absolute';
+    element.innerHTML= 'Continuar';
+    element.style.color = 'red';
+    
+    element.style.bottom = '1%';
+    element.onclick = showAlert;
+    footer.appendChild(element);
+
+}
+
+function showAlert(){
+    alert('Felicidades! Terminaste el juego!');
+}
